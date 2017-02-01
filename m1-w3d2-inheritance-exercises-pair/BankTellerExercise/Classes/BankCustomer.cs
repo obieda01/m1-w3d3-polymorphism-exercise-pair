be51ed3f6479e3d2519ddb481e867b3eef7ee0d4
Jacob Rutter <rutter.jacob@gmail.com>
2017-02-01 14:13:27 -0500

    Finished the bankteller project

---
+++ m1-w3d2-inheritance-exercises-pair/BankTellerExercise/Classes/BankCustomer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTellerExercise.Classes
+{
+    public class BankCustomer
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string address;
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        private string phoneNumber;
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        private BankAccount[]  accounts=new BankAccount[] { };
+
+        public BankAccount[]  Accounts
+        {
+            get { return accounts; }
+            set { accounts = value; }
+        }
+
+        public void AddAccount(BankAccount newAccount)
+        {
+
+            List<BankAccount> temp = Accounts.ToList<BankAccount>();
+            temp.Add(newAccount);
+            Accounts = temp.ToArray();
+
+        }
+        private bool isVip = false;
+        public bool IsVip
+        {
+            get { return isVipCustomer(); }
+            set { isVip = isVipCustomer(); }
+        }
+        public bool isVipCustomer()
+        {
+            int sum = 0;
+            for (int x=0; x<Accounts.Length; x++)
+            {
+                sum += Accounts[x].Balance.Dollars*100+ Accounts[x].Balance.Cents;
+            }
+            if (sum >= 2500000)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ m1-w3d2-inheritance-exercises-pair/BankTellerExerciseTests/Classes/BankCustomerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankTellerExercise.Classes;
+
+namespace BankTellerExerciseTests.Classes
+{
+    [TestClass]
+    public class BankCustomerTest
+    {
+        [TestMethod]
+        public void BankCustomer_AddAccountTests()
+        {
+            BankCustomer TestedInstanceBankCustomer = new BankCustomer();
+            BankAccount First = new BankAccount();
+            BankAccount Second = new BankAccount();
+            TestedInstanceBankCustomer.Accounts = new BankAccount[] { First, Second };    
+
+            BankCustomer Tested = new BankCustomer();
+            Tested.AddAccount(First);
+            Tested.AddAccount(Second);
+            Assert.AreEqual(TestedInstanceBankCustomer.Accounts.Length, Tested.Accounts.Length);
+
+        }
+        [TestMethod]
+        public void BankCustomer_isVIPTests()
+        {
+            BankCustomer TestedVip = new BankCustomer();
+            BankAccount First = new BankAccount();
+            BankAccount Second = new BankAccount();
+            TestedVip.Accounts = new BankAccount[] { First, Second, new BankAccount()};
+            TestedVip.Accounts[0].Deposit(new DollarAmount(100));
+            TestedVip.Accounts[1].Deposit(new DollarAmount(500));
+            TestedVip.Accounts[2].Deposit(new DollarAmount(1000));
+            Assert.IsFalse(TestedVip.isVipCustomer());
+            TestedVip.Accounts[0].Deposit(new DollarAmount(250000));
+            TestedVip.Accounts[1].Deposit(new DollarAmount(500000000));
+            TestedVip.Accounts[2].Deposit(new DollarAmount(1000000));
+            Assert.IsTrue(TestedVip.isVipCustomer());
+        }
+    }
+}
