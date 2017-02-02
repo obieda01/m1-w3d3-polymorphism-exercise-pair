9650f28249d4ac5302942e8c53e637dffa4a77f3
Ahmadgit config --global user.name Ahmadgit config --global user.name Ahmad <alakmo8@gmail.com>
2017-02-02 10:13:14 -0500

    a better implementaion for program.cs using polymorohism

--- m1-w3d3-polymorphism-exercise-pair/EmployeePayroll/Program.cs
+++ m1-w3d3-polymorphism-exercise-pair/EmployeePayroll/Program.cs
@@ -9,63 +9,40 @@ namespace EmployeePayroll
 {
     class Program
     {
+
+        public static void writeToConsole(string f, string m, string l)
+        {
+            Console.WriteLine(f.PadRight(35) + m.PadRight(35) + l);
+
+        }
+
         static void Main(string[] args)
         {
             Random r = new Random();
-            
-            List<HourlyWorker> hourlyWorkerList = new List<HourlyWorker>()
-            {
-                new HourlyWorker(25, "Logi", "Tech"),
-
-            };
-            List<SalaryWorker> salaryWorkerList = new List<SalaryWorker>() {
-
-                new SalaryWorker(104000, "John", "Mack"),
-
-             };
-            List<VolunteerWorker> volunteerWorkerList = new List<VolunteerWorker>() {
-                new VolunteerWorker("Hobs", "Carb"),
-             };
-
             int allHoursWorked = 0;
             double allPay = 0;
-            string a = "Employee".PadRight(40) + "Hours Worked".PadRight(40) + "pay";
-            Console.WriteLine(a+"\n");
 
-            foreach (HourlyWorker worker in hourlyWorkerList)
+            List<IWorker> iWorkerList = new List<IWorker>()
+            {
+                new HourlyWorker(25, "Logi", "Tech"),new SalaryWorker(104000, "John", "Mack"),
+                 new VolunteerWorker("Hobs", "Carb"),
+            };
+
+            writeToConsole("Employee", "Hours Worked", "pay");
+            Console.WriteLine("*********************************************************************************************\n");
+
+            foreach (IWorker worker in iWorkerList)
             {
                 int random = r.Next(0, 100);
                 allHoursWorked += random;
-                string s = worker.FirstName.PadRight(40) + random.ToString().PadRight(40) + worker.CalculateWeeklyPay(random);
-                Console.WriteLine(s);
+                writeToConsole(worker.FirstName, random.ToString(), worker.CalculateWeeklyPay(random).ToString());
+
                 allPay += worker.CalculateWeeklyPay(random);
             }
-            Console.WriteLine();
 
 
-
-
-            foreach (VolunteerWorker worker in volunteerWorkerList)
-            {
-                int random = r.Next(0, 100);
-                allHoursWorked += random;
-
-                Console.WriteLine(worker.FirstName + "      " + random + "         " + worker.CalculateWeeklyPay(random));
-                allPay += worker.CalculateWeeklyPay(random);
-            }
-            Console.WriteLine();
-            foreach (SalaryWorker worker in salaryWorkerList)
-            {
-                int random = r.Next(0, 100);
-                allHoursWorked += random;
-
-                Console.WriteLine(worker.FirstName + " " + random + "              " + worker.CalculateWeeklyPay(random));
-                allPay += worker.CalculateWeeklyPay(random);
-            }
-            Console.WriteLine();
-
-            Console.WriteLine("Total Hours:"+allHoursWorked);
-            Console.WriteLine("Total Pay: $"+ allPay);
+            Console.WriteLine("Total Hours:" + allHoursWorked);
+            Console.WriteLine("Total Pay: $" + allPay);
             Console.ReadKey();
         }
     }
